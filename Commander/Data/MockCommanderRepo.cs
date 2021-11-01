fb246390f45e4d2773fefef0feb2ade99909b124
mohamedebrahimselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2021-11-01 23:21:39 +0200

    Implement our Repository Interface (Mock Reposiory)

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data{
+    public class MockCommanderRepo : ICommandRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>{
+               new Command{Id = 0 , Platform = "dodo" , Line = "SoSo" , HowTo = "MoMo"},
+               new Command{Id = 0 , Platform = "dodo" , Line = "SoSo" , HowTo = "MoMo"},
+               new Command{Id = 0 , Platform = "dodo" , Line = "SoSo" , HowTo = "MoMo"}
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command{Id = 0 , Platform = "dodo" , Line = "SoSo" , HowTo = "MoMo"};
+            
+        }
+    }
+}
